b654a76d79aa822bc813bb6cbb4c8f3243f1ef76
nogchau <141125357+nogchau@users.noreply.github.com>
2025-04-11 03:36:56 +0700

    Update App.jsx

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,7 +1,17 @@
+import { Routes , Route } from "react-router-dom";
+import Navbar from "./components/navbar.component";
+
 const App = () => {
     return (
-        <h1>MERN Blogging website by modern web</h1>
-    )
+
+        <Routes>
+            <Route path="/" element={<Navbar />} />
+            <Route path="signin"element={<h1>Sign in Page</h1>}/>
+            <Route path="signup"element={<h1>Sign up Page</h1>}/>
+            
+        </Routes>
+        
+    );
 }
 
 export default App;
